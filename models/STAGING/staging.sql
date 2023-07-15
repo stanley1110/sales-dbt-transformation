2b6772e041a59674249ca5b63e3db951190b6b19
Stanley Edoh <edohemeka2@gmail.com>
2023-07-15 18:53:19 +0000

    created staging table 1

---
+++ models/STAGING/staging.sql
@@ -0,0 +1,12 @@
+select 
+order_id,order_date, order_status, order_customer_id,
+oi.order_item_product_id,oi.order_item_product_price,oi.order_item_quantity,oi.order_item_subtotal,p.product_name, p.product_price
+,c.customer_city,c.customer_lname,c.customer_fname,c.customer_state,c.customer_street,c.customer_zipcode,c.customer_id
+
+from {{ ref('orders') }} as o
+left join {{ ref('order_items') }} as oi
+on o.order_id = oi.order_item_order_id
+left join {{ ref('customers') }} as c
+on o.order_customer_id = c.customer_id
+left join {{ ref('products') }} as p
+on oi.order_item_product_id = p.product_id
