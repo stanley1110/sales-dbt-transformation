23644c3b6df9f81fb58606a1dc86fc43b2bd29bb
Stanley Edoh <edohemeka2@gmail.com>
2023-07-20 10:52:12 +0000

    update

--- models/REPORTING/product_revenue.sql
+++ models/REPORTING/product_revenue.sql
@@ -1,3 +1,9 @@
-select product_id, order_date, product_name, revenue
+select
+    product_id,
+    order_date,
+    product_name,
+    order_item_quantity,
+    order_item_product_price,
+    revenue
 from {{ ref("staging") }}
 where order_status = 'CLOSED'
